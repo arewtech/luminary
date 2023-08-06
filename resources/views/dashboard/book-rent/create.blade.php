6e445bff51a2eafd16f432671f989a6ae34520bc
Maman` <75215687+JfeStudio@users.noreply.github.com>
2023-08-06 23:13:55 +0700

    redesign book rent

--- resources/views/dashboard/book-rent/create.blade.php
+++ resources/views/dashboard/book-rent/create.blade.php
@@ -3,7 +3,7 @@
     <main id="main" class="main">
 
         <!-- ======= Page Title ======= -->
-        <div class="pagetitle d-flex flex-column justify-content-center align-items-center">
+        <div class="pagetitle">
             <h1>Book Rent</h1>
             <nav>
                 <ol class="breadcrumb">
@@ -14,50 +14,81 @@
         </div>
         <!-- End Page Title -->
 
-        <div class="card mb-3 col-5 m-auto">
-
-            <div class="card-body">
-
-                <div class="pt-4 pb-2">
-                    <h5 class="card-title text-center pb-0 fs-4">Login to Your Account</h5>
-                    <p class="text-center small">Enter your username & password to login</p>
+        <section class="section faq">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="pt-4 pb-2">
+                                <h5 class="card-title text-center pb-0 fs-4">Create Book Rent 📖</h5>
+                                <p class="text-center small">
+                                    Please fill in the form below to create a book rent.
+                                </p>
+                            </div>
+                            @foreach ($errors->all() as $item)
+                                {{ $item }}
+                            @endforeach
+                            <form action="{{ route('book-rent.store') }}" method="post" class="row g-3 needs-validation">
+                                @csrf
+                                <div class="col-12">
+                                    <label for="selectUsers" class="form-label">Name / Username</label>
+                                    <select id="selectUsers" name="user_id" class="form-select">
+                                        <option selected="" disabled>select users</option>
+                                        @foreach ($users as $user)
+                                            <option value="{{ $user->id }}">{{ $user->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="col-12">
+                                    <label for="selectBooks" class="form-label">Book Rent</label>
+                                    <select id="selectBooks" name="book_id" class="form-select">
+                                        <option selected="" disabled>select books</option>
+                                        @foreach ($books as $book)
+                                            <option value="{{ $book->id }}">{{ $book->title }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="col-12">
+                                    <button class="btn btn-primary w-100" type="submit">Rent Book</button>
+                                </div>
+                                <div class="col-12">
+                                    <p class="small mb-0">
+                                        <span class="text-danger">*</span> <span class="fw-bold">Note:</span> Rent
+                                        book
+                                        will be automatically returned after 3 days.
+                                    </p>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
                 </div>
-                @foreach ($errors->all() as $item)
-                    {{ $item }}
-                @endforeach
-                <form action="{{ route('book-rent.store') }}" method="post" class="row g-3 needs-validation">
-                    @csrf
-                    <div class="col-12">
-                        <label for="selectUsers" class="form-label">Name / Username</label>
-                        <select id="selectUsers" name="user_id" class="form-select">
-                            <option selected="" disabled>select users</option>
-                            @foreach ($users as $user)
-                                <option value="{{ $user->id }}">{{ $user->name }}</option>
-                            @endforeach
-                        </select>
+                <div class="col-lg-6">
+                    <div class="card basic">
+                        <div class="card-body">
+                            <h5 class="card-title">
+                                <i class="bi bi-question-circle"></i> Frequently Asked Questions
+                            </h5>
+                            <div>
+                                <h6>1. Nisi ut ut exercitationem voluptatem esse sunt rerum?</h6>
+                                <p>Saepe perspiciatis ea. Incidunt blanditiis enim mollitia qui voluptates. Id rem nulla
+                                    tenetur
+                                    nihil in unde rerum. Quae consequatur placeat qui cumque earum eius omnis quos.</p>
+                            </div>
+                            <div class="pt-2">
+                                <h6>2. Reiciendis dolores repudiandae?</h6>
+                                <p>Id ipsam non ut. Placeat doloremque deserunt quia tenetur inventore laboriosam dolores
+                                    totam
+                                    odio. Aperiam incidunt et. Totam ut quos sunt atque modi molestiae dolorem.</p>
+                            </div>
+                            <div class="pt-2">
+                                <h6>3. Qui qui reprehenderit ut est illo numquam voluptatem?</h6>
+                                <p>Enim inventore in consequuntur ipsam voluptatem consequatur beatae. Nostrum consequuntur
+                                    voluptates et blanditiis.</p>
+                            </div>
+                        </div>
                     </div>
-
-                    <div class="col-12">
-                        <label for="selectBooks" class="form-label">Book Rent</label>
-                        <select id="selectBooks" name="book_id" class="form-select">
-                            <option selected="" disabled>select books</option>
-                            @foreach ($books as $book)
-                                <option value="{{ $book->id }}">{{ $book->title }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-
-                    <div class="col-12">
-                        <button class="btn btn-primary w-100" type="submit">Rent Book</button>
-                    </div>
-                    <div class="col-12">
-                        <p class="small mb-0">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus odit earum
-                            quae.</a>
-                        </p>
-                    </div>
-                </form>
-
+                </div>
             </div>
-        </div>
+        </section>
     </main>
 @endsection
